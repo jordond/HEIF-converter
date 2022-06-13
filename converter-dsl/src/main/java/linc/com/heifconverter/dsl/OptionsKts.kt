0ff7ffe5170e7db622ff66e3ae1ab10d633c789c
Jordon de Hoog <jordon.dehoog@gmail.com>
2022-06-13 13:02:14 -0400

    add comments to HeifConverter.Options.build

--- converter-dsl/src/main/java/linc/com/heifconverter/dsl/OptionsKts.kt
+++ converter-dsl/src/main/java/linc/com/heifconverter/dsl/OptionsKts.kt
@@ -2,6 +2,24 @@ package linc.com.heifconverter.dsl
 
 import linc.com.heifconverter.HeifConverter
 
+/**
+ * A DSL builder method for creating an instance of [HeifConverter.Options].
+ *
+ * Example:
+ *
+ * ```
+ * val options = HeifConverter.Options.build {
+ *     saveResultImage = true
+ *     outputQuality(50)
+ *     outputDirectory(context.cacheDir)
+ * }
+ *
+ * val result = HeifConverter.create(context, "https://sample.com/image.heic", options).convert()
+ * ```
+ *
+ * @param[block] A lambda scoped to [HeifConverterDsl] for creating an [HeifConverter.Options].
+ * @return The built [HeifConverter.Options] object.
+ */
 public fun HeifConverter.Options.Companion.build(
     block: HeifConverterDsl.() -> Unit = {},
 ): HeifConverter.Options = InternalHeifConverterDsl().apply(block).options
